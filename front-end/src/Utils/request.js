1e71d3d9581de88f437e19ca919f7038bd0b8742
Nguyễn Ngọc Luân <luannn308@gmail.com>
2024-01-13 15:48:54 +0700

    Done

--- front-end/src/Utils/request.js
+++ front-end/src/Utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: "http://192.168.1.206:3000",
+    baseURL: "http://localhost:3000",
 });
 export const get = async (path, option = {}) => {
     const response = await request.get(path, option);
